4f3a0431f63046e3c469b2712cf2c3f40d7ae4ba
Nishant-66 <kumarnishant26346@gmail.com>
2024-06-02 20:54:47 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+
+      const heading =React.createElement('div',{},[[
+        React.createElement('h1',{},"Hello World"),
+        React.createElement('h1',{},"Hello Friends")
+      ],
+      [
+        React.createElement('h2',{},"Hello World"),
+        React.createElement('h2',{},"Hello Friends")
+      ]
+    ])
+    const root=ReactDOM.createRoot(document.getElementById('root'));
+    root.render(heading);
+     
